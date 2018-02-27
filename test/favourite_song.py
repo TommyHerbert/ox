6760599f0175a46cd5414c7b167b28b46fa47522
Tommy Herbert <tommherbert@gmail.com>
2018-02-27 10:46:28 +0000

    Use Python test framework for unit tests.

--- test/favourite_song.py
+++
@@ -1,3 +0,0 @@
-import main.ox
-ox = main.ox.Ox()
-print ox.tell("What's your favourite Adele song?")
---
+++ test/test_ox.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from main.ox import Ox
+
+
+class TestOx(TestCase):
+    def test_song(self):
+        self.assertEqual('Hello', Ox().tell("What's your favourite song?"))
+
+    def test_adele_song(self):
+        self.assertEqual('Hello', Ox().tell("What's your favourite Adele song?"))
