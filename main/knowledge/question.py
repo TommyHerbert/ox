e0188a3e066d770c2b0eca6776c6c248e8650a18
Tommy Herbert <tommherbert@gmail.com>
2018-03-06 14:13:04 +0000

    REFACTOR: Signature of Question.get_logical_form now matches base method.

--- main/knowledge/question.py
+++ main/knowledge/question.py
@@ -8,7 +8,9 @@ class Question(Category):
     def __init__(self):
         Category.__init__(self)
 
-    def get_logical_form(self, input_string, reader):
+    def get_logical_form(self, input_string=None, reader=None):
+        if None in [input_string, reader]:
+            return None
         question_logic = self.get_question_logic(input_string, reader)
         return Expectation(question_logic) if question_logic else None
 
