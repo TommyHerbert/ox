30135a247ced3a06d4ab1b3c6d4f1a84bddd0629
Tommy Herbert <tommyherbert@gmail.com>
2018-08-08 11:19:59 +0100

    Add test module for self-writing feature.

---
+++ test/test_self_writing.py
@@ -0,0 +1,39 @@
+import unittest
+from app import create_app, db
+from app.models import Speaker, Conversation, Utterance
+from config import Config
+from knowledge.song import Song
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class BackEndTests(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+        self.ox = Speaker(email='project.ox.mail@gmail.com')
+        self.test_user = Speaker(email='test@users.com')
+        for s in [self.ox, self.test_user]:
+            db.session.add(s)
+        db.session.commit()
+
+        self.conversation = Conversation.create()
+        for s in [self.ox, self.test_user]:
+            self.conversation.speakers.append(s)
+        db.session.add(self.conversation)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_get_import_statement(self):
+        expected = 'from knowledge.song import Song'
+        self.assertEqual(expected, Song().get_import_statement())
+
