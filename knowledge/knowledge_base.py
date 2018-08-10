07430a42d1607eaffef55badd23d7335f9bb1fb9
Tommy Herbert <tommyherbert@gmail.com>
2018-08-10 12:07:54 +0100

    Remove obsolete code.

--- knowledge/knowledge_base.py
+++ knowledge/knowledge_base.py
@@ -10,19 +10,3 @@ class KnowledgeBase:
     def is_empty(self):
         return self.categories + self.things + self.relations == []
 
-    # TODO: maybe the Concept class should handle some of this logic
-    def write(self, path):
-        imports = []
-        instantiations = []
-        for concept in self.categories + self.things:
-            import_template = 'from knowledge.{} import {}'
-            class_name = category.__class__.__name__
-            module_name = headline_to_snake(class_name)
-            imports.append(import_template.format(module_name, class_name))
-            instantiation_template = '{} = {}()'
-            instantiations.append('{} = {}()'.format(module_name, class_name))
-            
-        imports.append('from knowledge.relation import Relation')
-        imports.append('from utils.case import headline_to_snake')
-        # TODO
-
