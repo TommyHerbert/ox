056c076ed4cd647439b6e9869a992a48195766ed
Tommy Herbert <tommyherbert@gmail.com>
2018-08-10 12:30:50 +0100

    WIP: Tests for self-writing.

--- test/test_self_writing.py
+++ test/test_self_writing.py
@@ -6,6 +6,12 @@ from knowledge.knowledge_base_populator import KnowledgeBasePopulator
 
 
 class TestSelfWriting(unittest.TestCase):
+    def setUp(self):
+        pass # TODO: create test_output package with __init__.py
+
+    def tearDown(self):
+        pass # TODO: destroy test_output package and all its contents
+
     def test_get_import_statement(self):
         expected = 'from foo.bar.baz.qux.knowledge.song import Song'
         actual = Song().get_import_statement('foo/bar/baz/qux')
@@ -25,4 +31,25 @@ class TestSelfWriting(unittest.TestCase):
         self.assertTrue(knowledge_base.is_empty())
         KnowledgeBasePopulator.populate(knowledge_base)
         self.assertFalse(knowledge_base.is_empty())
+
+    def test_matches(self):
+        knowledge_base1 = KnowledgeBase()
+        knowledge_base2 = KnowledgeBase()
+        knowledge_base3 = KnowledgeBase()
+        knowledge_base1.things.append(Adele())
+        knowledge_base2.things.append(Adele())
+        knowledge_base3.categories.append(Song())
+        self.assertTrue(knowledge_base1.matches(knowledge_base2))
+        self.assertFalse(knowledge_base1.matches(knowledge_base3))
+
+    #def test_export_populator(self):
+    #    knowledge_base1 = KnowledgeBase()
+    #    KnowledgeBasePopulator.populate(knowledge_base1)
+    #    knowledge_base1.export_populator('test_output/iteration2')
+    #    from test_output.iteration2.knowledge \
+    #        import KnowledgeBasePopulator as KnowledgeBasePopulator2
+    #    knowledge_base2 = KnowledgeBase()
+    #    KnowledgeBasePopulator2.populate(knowledge_base2)
+    #    self.assertTrue(knowledge_base1.matches(knowledge_base2))
+        # TODO
 
