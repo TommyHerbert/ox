b32dfbf1b6e0fb0449691bf50500820be2b46c09
Tommy Herbert <tommyherbert@gmail.com>
2018-08-13 10:54:28 +0100

    WIP: Logic for exporting a new version of the knowledge base populator.

--- knowledge/knowledge_base.py
+++ knowledge/knowledge_base.py
@@ -1,4 +1,7 @@
 from utils.case import headline_to_snake
+from os.path import join
+from os import makedirs
+from pathlib import Path
 
 
 class KnowledgeBase:
@@ -11,7 +14,17 @@ class KnowledgeBase:
         return self.categories + self.things + self.relations == []
     
     def export_populator(self, path):
-        pass # TODO
+        knowledge_path = join(path, 'knowledge')
+        makedirs(knowledge_path)
+        Path(join(knowledge_path, '__init__.py')).touch()
+        populator_path = join(knowledge_path, 'knowledge_base_populator.py')
+        with open(populator_path, 'w') as populator_file:
+            for concept in self.categories + self.things:
+                populator_file.write(concept.get_import_statement(path) + '\n')
+                # TODO: extract package path creation from
+                # get_import_statement
+            # TODO
+
 
     def matches(self, knowledge_base):
         pass # TODO
