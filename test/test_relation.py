c37b9d5a738c204aebfca2ce1da50073d257018c
Tommy Herbert <tommyherbert@gmail.com>
2018-09-19 18:57:04 +0100

    Tests for comparing relations.

--- test/test_relation.py
+++ test/test_relation.py
@@ -1,11 +1,29 @@
 import unittest
+from knowledge.adele import Adele
+from knowledge.singer import Singer
+from knowledge.hello import Hello
+from knowledge.someone_like_you import SomeoneLikeYou
 from knowledge.relation import Relation
 
 
 class TestRelation(unittest.TestCase):
     def test_equality(self):
-        pass # TODO
+        adele1 = Adele()
+        adele2 = Adele()
+        singer1 = Singer()
+        singer2 = Singer()
+        adeleSinger1 = Relation('is_a', (adele1, singer1))
+        adeleSinger2 = Relation('is_a', (adele2, singer2))
+        self.assertEqual(adeleSinger1, adeleSinger2)
+
+        singerAdele = Relation('is_a', (singer1, adele1))
+        self.assertNotEqual(adeleSinger1, singerAdele)
 
     def test_less_than(self):
-        pass # TODO
+        adele = Adele()
+        hello = Hello()
+        someone_like_you = SomeoneLikeYou()
+        hello_relation = Relation('sang', (adele, hello))
+        someone_relation = Relation('sang', (adele, someone_like_you))
+        self.assertTrue(hello_relation < someone_like_you)
 
