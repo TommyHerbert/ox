8822cd13ae23b45505a43f8e21675cc9ebe04fe6
Tommy Herbert <tommyherbert@gmail.com>
2018-10-02 14:37:04 +0100

    Remove knowledge package name from Concept.get_import_statement.

--- knowledge/concept.py
+++ knowledge/concept.py
@@ -32,10 +32,10 @@ class Concept:
         return headline_to_snake(self.get_class_name())
     
     def get_import_statement(self, path=''):
-        package_path = to_package_path(path, final_dot=True)
-        return 'from {}knowledge.{} import {}'.format(package_path,
-                                                      self.get_module_name(),
-                                                      self.get_class_name())
+        package = to_package_path(path) # TODO: get rid of optional param?
+        module = self.get_module_name()
+        class_name = self.get_class_name()
+        return 'from {}.{} import {}'.format(package, module, class_name)
 
     def get_instantiation_statement(self):
         return '{} = {}()'.format(self.get_module_name(),
