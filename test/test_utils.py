632aebc9e7c2aca947c98766226dec7a08815854
Tommy Herbert <tommyherbert@gmail.com>
2018-10-03 17:03:49 +0100

    Remove old utility function and its test.

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,8 +1,7 @@
 import unittest
 from utils.paths import to_package_path
 from utils.lists import sorted_copy
-from utils.knowledge import create_knowledge_package, \
-                            create_unique_package, \
+from utils.knowledge import create_unique_package, \
                             create_empty_knowledge_package, \
                             copy_knowledge_package
 from os import makedirs
@@ -39,13 +38,6 @@ class TestUtils(unittest.TestCase):
         list2 = sorted_copy(list1)
         self.assertEqual([3, 2, 1], list1)
         self.assertEqual([1, 2, 3], list2)
-
-    # TODO: this one will become obsolete
-    def test_create_knowledge_package(self):
-        unique_id = create_knowledge_package(OUTPUT_DIR)
-        path = join(OUTPUT_DIR, unique_id)
-        self.assertTrue(is_package(path))
-        self.assertTrue(is_package(join(path, 'knowledge')))
 
     def test_create_unique_package(self):
         path = join(OUTPUT_DIR, create_unique_package(OUTPUT_DIR))
--- utils/knowledge.py
+++ utils/knowledge.py
@@ -5,17 +5,6 @@ from uuid import uuid4
 from shutil import copytree
 
 
-# TODO: this one will become obsolete
-def create_knowledge_package(path):
-    unique_id = str(uuid4())
-    knowledge_path = join(path, unique_id, 'knowledge')
-    makedirs(knowledge_path)
-    Path(join(path, '__init__.py')).touch()
-    Path(join(path, unique_id, '__init__.py')).touch()
-    Path(join(knowledge_path, '__init__.py')).touch()
-    return unique_id
-
-
 def create_unique_package(path):
     unique_id = str(uuid4())
     extended_path = join(path, unique_id)
