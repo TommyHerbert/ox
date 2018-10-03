dd0642e4562a012f890b5dbab9ae876506d0e2bf
Tommy Herbert <tommyherbert@gmail.com>
2018-10-03 16:06:18 +0100

    Fix missing import statement in utils tests.

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,7 +1,10 @@
 import unittest
 from utils.paths import to_package_path
 from utils.lists import sorted_copy
-from utils.knowledge import create_knowledge_package
+from utils.knowledge import create_knowledge_package, \
+                            create_unique_package, \
+                            create_empty_knowledge_package, \
+                            copy_knowledge_package
 from os import makedirs
 from os.path import join, exists, isdir, isfile
 from shutil import rmtree
@@ -22,13 +25,11 @@ class TestUtils(unittest.TestCase):
 
     def test_to_package_path_default_behaviour(self):
         self.assertEqual('', to_package_path(''))
-
         directory_path = join('foo', 'bar', 'baz')
         self.assertEqual('foo.bar.baz', to_package_path(directory_path))
 
     def test_to_package_path_with_final_dot(self):
         self.assertEqual('', to_package_path('', final_dot=True))
-
         expected = 'foo.bar.baz.'
         actual = to_package_path(join('foo', 'bar', 'baz'), final_dot=True)
         self.assertEqual(expected, actual)
