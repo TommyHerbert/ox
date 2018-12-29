92d8975783049ac4a6efe509f58977269f5f7e55
Tommy Herbert <tommyherbert@gmail.com>
2018-12-29 17:24:23 +0000

    Methods for getting and removing droplets.

--- test/load_balancer_experiment.py
+++ test/load_balancer_experiment.py
@@ -1,6 +1,38 @@
 import time
+from threading import Thread
+import requests
+from json import loads
+
+path_prefix = 'https://api.digitalocean.com/v2/load_balancers/'
 
 
-def print_time():
-    print(time.ctime(time.time()))
+def get_time_string():
+    return time.ctime(time.time())
+
+
+def build_headers(token):
+    return {'Authorization': 'Bearer ' + token}
+
+
+def remove_droplet(balancer_id, droplet_id, headers):
+    print('time before removing droplet: ' + get_time_string())
+    body = {'droplet_ids': [droplet_id]}
+    path = path_prefix + balancer_id + '/droplets'
+    requests.delete(path, headers=headers, json=body)
+    print('time after removing droplet: ' + get_time_string())
+
+
+def get_droplets(balancer_id, headers):
+    print('time before getting droplets: ' + get_time_string())
+    response = requests.get(path_prefix + balancer_id, headers=headers).text
+    print('droplets: ' + str(loads(response)['load_balancer']['droplet_ids']))
+    print('time after getting droplets: ' + get_time_string())
+
+
+class RemovalThread()
+
+
+def run(balancer_id, droplet_id, headers):
+    print 'starting removal thread'
+    
 
