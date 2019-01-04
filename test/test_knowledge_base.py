2a0ee2c85c063d5ba74ae002c728bfc1e7520a20
Tommy Herbert <tommyherbert@gmail.com>
2019-01-04 10:28:18 +0000

    Test for merging knowledge bases

--- test/test_knowledge_base.py
+++ test/test_knowledge_base.py
@@ -4,6 +4,9 @@ from knowledge.knowledge_base_populator import KnowledgeBasePopulator
 from knowledge.relation import Relation
 from knowledge.myself import Myself
 from knowledge.adele import Adele
+from knowledge.hello import Hello
+from knowledge.singer import Singer
+from knowledge.song import Song
 
 
 class TestKnowledgeBase(unittest.TestCase):
@@ -35,4 +38,31 @@ class TestKnowledgeBase(unittest.TestCase):
         self.assertEqual(initial_relations_count + 1, relations_count)
         relation = Relation(name, args)
         self.assertIn(relation, knowledge_base.relations)
+
+    def test_merge(self):
+        adele = Adele()
+        hello = Hello()
+        singer = Singer()
+        song = Song()
+
+        base1 = KnowledgeBase()
+        base2 = KnowledgeBase()
+        expected = KnowledgeBase()
+
+        base1.add_thing(adele)
+        base1.categories.append(singer)
+        base1.add_relation('is_a', (adele, singer))
+
+        base2.add_thing(adele)
+        base2.add_thing(hello)
+        base2.categories.append(song)
+        base2.add_relation('is_a', (hello, song))
+
+        expected.add_thing(adele)
+        expected.things = [adele, hello]
+        expected.categories = [singer, song]
+        expected.add_relation('is_a', (adele, singer))
+        expected.add_relation('is_a', (hello, song))
+
+        self.assertTrue(expected.matches(base1.merge(base2)))
 
