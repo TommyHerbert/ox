726ee21dd0feaaa21c86bea442bfaf1d1e57c413
Tommy Herbert <tommyherbert@gmail.com>
2019-01-05 08:27:01 +0000

    Check number of droplets in load balancer.

--- scripts/cds1.py
+++ scripts/cds1.py
@@ -1,4 +1,6 @@
 from sys import argv
+from requests import get
+from json import loads
 
 usage = 'usage: python cds1.py <authorization token> ' + \
         '<load balancer ID> <droplet ID>'
@@ -6,5 +8,12 @@ usage = 'usage: python cds1.py <authorization token> ' + \
 if len(argv) != 4:
     print(usage)
 token, balancer_id, droplet_id = argv[1:]
+path_prefix = 'https://api.digitalocean.com/v2/load_balancers/'
+path = path_prefix + balancer_id
+headers = {'Authorization': 'Bearer ' + token}
+response = loads(get(path, headers=headers).text)
+droplet_ids = response['load_balancer']['droplet_ids']
+print('there are {} droplets in the load balancer'.format(len(droplet_ids)))
+
 # TODO
 
