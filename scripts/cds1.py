7e4b609950ff79b21ed2b78cd3514ce95a3b44bd
Tommy Herbert <tommyherbert@gmail.com>
2019-01-07 12:26:30 +0000

    Wait for load balancer pool to populate.

--- scripts/cds1.py
+++ scripts/cds1.py
@@ -1,29 +1,28 @@
 from sys import argv
 from requests import get
 from json import loads
+from time import sleep
 
 # The load balancer pool must always have at least this many app hosts.
 MINIMUM_HOSTS = 1
 
-usage = 'usage: python cds1.py <authorization token> ' + \
+
+def count_droplets(path, headers):
+    response = loads(get(path, headers=headers).text)
+    return len(response['load_balancer']['droplet_ids'])
+
+
+USAGE = 'usage: python cds1.py <authorization token> ' + \
         '<load balancer ID> <droplet ID>'
 
 if len(argv) != 4:
-    print(usage)
+    print(USAGE)
 token, balancer_id, droplet_id = argv[1:]
 path_prefix = 'https://api.digitalocean.com/v2/load_balancers/'
 path = path_prefix + balancer_id
 headers = {'Authorization': 'Bearer ' + token}
-response = loads(get(path, headers=headers).text)
-droplet_ids = response['load_balancer']['droplet_ids']
-droplet_count = len(droplet_ids)
-print('there are {} droplets in the load balancer'.format(droplet_count))
-if droplet_count > MINIMUM_HOSTS:
-    print('So we can afford to remove this one temporarily.')
-else:
-    message = 'So we must wait for more droplets to add ' + \
-              'themselves before we remove this one.'
-    print(message)
-
-# TODO
+while count_droplets(path, headers) <= MINIMUM_HOSTS:
+    print('Insufficient droplets in load balancer pool. Waiting for more.')
+    sleep(5)
+print('There are now enough droplets. We can remove this one.')
 
