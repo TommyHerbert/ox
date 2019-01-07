c1b489ccbf1d31dc8d10f1af93fd677330958dbc
Tommy Herbert <tommyherbert@gmail.com>
2019-01-07 12:10:27 +0000

    Compare droplet count to minimum requirement.

--- scripts/cds1.py
+++ scripts/cds1.py
@@ -2,6 +2,9 @@ from sys import argv
 from requests import get
 from json import loads
 
+# The load balancer pool must always have at least this many app hosts.
+MINIMUM_HOSTS = 1
+
 usage = 'usage: python cds1.py <authorization token> ' + \
         '<load balancer ID> <droplet ID>'
 
@@ -13,7 +16,14 @@ path = path_prefix + balancer_id
 headers = {'Authorization': 'Bearer ' + token}
 response = loads(get(path, headers=headers).text)
 droplet_ids = response['load_balancer']['droplet_ids']
-print('there are {} droplets in the load balancer'.format(len(droplet_ids)))
+droplet_count = len(droplet_ids)
+print('there are {} droplets in the load balancer'.format(droplet_count))
+if droplet_count > MINIMUM_HOSTS:
+    print('So we can afford to remove this one temporarily.')
+else:
+    message = 'So we must wait for more droplets to add ' + \
+              'themselves before we remove this one.'
+    print(message)
 
 # TODO
 
