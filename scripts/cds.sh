df2ffa329052ee5cd20f2a57a706a2c9f27aa5a1
Tommy Herbert <tommyherbert@gmail.com>
2019-01-15 15:10:29 +0000

    Continuous Development Service: call Python script from shell script.

--- scripts/cds.sh
+++ scripts/cds.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo `date` 'activity detected in NewKnowledge folder' >> /home/oxadmin/ox/cds.log
+source /home/oxadmin/ox/venv/bin/activate
+python /home/oxadmin/ox/scripts/cds1.py $DIGITAL_OCEAN_TOKEN $LOAD_BALANCER_ID $DROPLET_ID
+echo `date` 'called cds1.py' >> /home/oxadmin/ox/cds.log
