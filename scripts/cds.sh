269ebc47a01a7efd63a0f4ae5579d2111223e068
Tommy Herbert <tommyherbert@gmail.com>
2019-01-29 15:32:04 +0000

    Bring the CDS bash script up to date with the Python components

--- scripts/cds.sh
+++ scripts/cds.sh
@@ -4,6 +4,4 @@ source /home/oxadmin/ox/venv/bin/activate
 python /home/oxadmin/ox/scripts/cds1.py $DIGITAL_OCEAN_TOKEN $LOAD_BALANCER_ID $DROPLET_ID
 echo `date` 'called cds1.py' >> /home/oxadmin/ox/cds.log
 python /home/oxadmin/ox/scripts/cds2.py
-rm -r /home/oxadmin/ox/knowledge
-rm -r /home/oxamin/ox/new_knowledge/knowledge
-mv /tmp/ox/knowledge /home/oxadmin/ox
+echo `date` 'called cds2.py' >> /home/oxadmin/ox/cds.log
