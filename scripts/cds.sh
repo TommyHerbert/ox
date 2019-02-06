e8428bf8e0fe979bff67d725af56e277dca141df
Tommy Herbert <tommyherbert@gmail.com>
2019-02-06 10:18:26 +0000

    Bounce the service when the source code changes.

--- scripts/cds.sh
+++ scripts/cds.sh
@@ -3,5 +3,9 @@ echo `date` 'activity detected in NewKnowledge folder' >> /home/oxadmin/ox/cds.l
 source /home/oxadmin/ox/venv/bin/activate # TODO: Do I need this?
 python /home/oxadmin/ox/scripts/cds1.py $DIGITAL_OCEAN_TOKEN $LOAD_BALANCER_ID $DROPLET_ID &>> /home/oxadmin/ox/logs/cds1.log
 echo `date` 'called cds1.py' >> /home/oxadmin/ox/cds.log
+supervisorctl stop ox
+echo `date` 'stopped the app server' >> /home/oxadmin/ox/cds.log
 python /home/oxadmin/ox/scripts/cds2.py &>> /home/oxadmin/ox/logs/cds2.log
 echo `date` 'called cds2.py' >> /home/oxadmin/ox/cds.log
+supervisorctl start ox
+echo `date` 'started the app server' >> /home/oxadmin/ox/cds.log
