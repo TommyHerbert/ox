5f7e787a4a0b96a4ac9c09bf2479b36ca74a4032
Tommy Herbert <tommyherbert@gmail.com>
2019-02-06 10:39:30 +0000

    Log the output of supervisorctl commands.

--- scripts/cds.sh
+++ scripts/cds.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo `date` 'activity detected in NewKnowledge folder' >> /home/oxadmin/ox/cds.log
+echo `date` 'activity detected in NewKnowledge folder' >> /home/oxadmin/ox/cds.log # TODO: put in logs directory
 source /home/oxadmin/ox/venv/bin/activate # TODO: Do I need this?
 python /home/oxadmin/ox/scripts/cds1.py $DIGITAL_OCEAN_TOKEN $LOAD_BALANCER_ID $DROPLET_ID &>> /home/oxadmin/ox/logs/cds1.log
 echo `date` 'called cds1.py' >> /home/oxadmin/ox/cds.log
-supervisorctl stop ox
+supervisorctl stop ox &>> /home/oxadmin/ox/cds.log
 echo `date` 'stopped the app server' >> /home/oxadmin/ox/cds.log
 python /home/oxadmin/ox/scripts/cds2.py &>> /home/oxadmin/ox/logs/cds2.log
 echo `date` 'called cds2.py' >> /home/oxadmin/ox/cds.log
-supervisorctl start ox
+supervisorctl start ox &>> /home/oxadmin/ox/cds.log
 echo `date` 'started the app server' >> /home/oxadmin/ox/cds.log
