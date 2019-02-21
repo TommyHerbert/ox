bd8e6feacae2ff3c00237df68d969fa977d02504
Tommy Herbert <tommyherbert@gmail.com>
2019-02-21 16:43:16 +0000

    Test for learning strategy.

---
+++ test/test_learning_strategy.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from knowledge.knowledge_base import KnowledgeBase
+from knowledge.naive_learning_strategy import select
+from knowledge.adele import Adele
+from knowledge.singer import Singer
+from knowledge.relation import Relation
+
+
+class TestLearningStrategy(TestCase):
+    def test_learning_strategy(self):
+        knowledge_base = KnowledgeBase()
+        adele = Adele()
+        singer = Singer()
+        relation = Relation('is_a', (adele, singer))
+        new_elements = ((adele), (singer), (relation))
+        self.assertEqual(new_elements, select(new_elements, knowledge_base))
+
